7c296ad701442e851811d6e7170936c960228ad9
Nirjhar Choudhury <nirjhar_choudhury@trimble.com>
2021-03-29 18:49:37 -0400

    First checkin

---
+++ Elasticsearch.Demo/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elasticsearch.Demo
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Address { get; set; }
+    }
+}
